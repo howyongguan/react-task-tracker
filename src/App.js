a4062e193e89ea0514460106dfab980e115274b4
howyongguan <howyongyuan1016@gmail.com>
2021-06-06 10:24:11 +0800

    tracker - initializing repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
-function App() {
+
+const App = () => {
+  const [tasks, setTasks]=useState([
+      {
+        id: 1,
+        text: "Doctor Appointment",
+        day: "Feb 5th at 2.30pm",
+        reminder: true,
+      },
+    
+      {
+        id: 2,
+        text: "Meeting at 6pm",
+        day: "Feb 6th at 1.30pm",
+        reminder: true,
+      },
+    
+      {
+        id: 3,
+        text: "Food Shopping ",
+        day: "Feb 5th at 11.30pm",
+        reminder: true,
+      },
+    ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="Container">
+      <Header />
+      <Tasks tasks={tasks} />
 
-export default App;
+    
+     </div>
+   )
+   }
+
+ export default App;
